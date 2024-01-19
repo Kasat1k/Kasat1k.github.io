a349313897ae7e5c435cf178063f2177bb974848
Kasat1k <104501299+Kasat1k@users.noreply.github.com>
2024-01-19 17:15:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const patchButBTN = document.getElementsByClassName("btn btn-primary Patch_buy_BTN")
-const btn=document.getElementsByName("btn btn-primary Patch_buy_BTN")
-patchButBTN.onclick = function () {
-    console.log("work")
-  window.alert(
-    'На даний момент ця функція не доступна . Ви можете перейти на інстаграм-сторінку по посиланню внизу сторінки'
-  );
-};
+var patchButButtons = document.querySelectorAll(".Patch_buy_BTN");
+
+patchButButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    window.alert(
+      "На даний момент ця функція не доступна. Ви можете перейти на інстаграм-сторінку за ніком @VELCRO_PATHCES_ST або нажавши на логотип інстаграму внизу сторінки"
+    );
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all card elements
+  const cards = document.querySelectorAll(".card");
+
+  // Find the maximum height among all cards
+  let maxHeight = 0;
+  cards.forEach((card) => {
+    const cardHeight = card.clientHeight;
+    if (cardHeight > maxHeight) {
+      maxHeight = cardHeight;
+    }
+  });
+
+  // Set the height of all cards to the maximum height
+  cards.forEach((card) => {
+    card.style.height = maxHeight + "px";
+  });
+});
