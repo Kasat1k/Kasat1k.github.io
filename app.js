9afce2fd5d5f6ad71eb624f9ffdd09793b5772e0
Kasat1k <104501299+Kasat1k@users.noreply.github.com>
2024-01-19 15:12:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const patchButBTN = document.getElementsByClassName("btn btn-primary Patch_buy_BTN")
+const btn=document.getElementsByName("btn btn-primary Patch_buy_BTN")
+patchButBTN.onclick = function () {
+    console.log("work")
+  window.alert(
+    'На даний момент ця функція не доступна . Ви можете перейти на інстаграм-сторінку по посиланню внизу сторінки'
+  );
+};
